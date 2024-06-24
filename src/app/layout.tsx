7ee7a98ec91db2f5297991f6fca8acfd9409ca1c
iypavlov <iy.pavlov@yandex.ru>
2024-06-24 21:37:51 +0300

    twa script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,12 @@
-import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import '/node_modules/modern-normalize/modern-normalize.css';
 import './globals.scss';
 import React from 'react';
 import styles from './layout.module.scss';
+import Script from 'next/script';
+import { TELEGRAM_WEP_APP_SCRIPT_URL } from '@/constants/common';
 
 const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'Mystic Ball',
-};
 
 export default function RootLayout({
   children,
@@ -18,6 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>Mystic Ball</title>
+        <Script src={TELEGRAM_WEP_APP_SCRIPT_URL} />
+      </head>
       <body className={inter.className}>
         <main className={styles.main}>{children}</main>
       </body>
---
+++ src/constants/common.ts
@@ -0,0 +1,2 @@
+export const TELEGRAM_WEP_APP_SCRIPT_URL =
+  'https://telegram.org/js/telegram-web-app.js';
