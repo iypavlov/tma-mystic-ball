ae1fef7ae6a040ea5dc567689dac62421380e18a
iypavlov <iy.pavlov@yandex.ru>
2024-06-24 23:15:53 +0300

    added github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import { MysticBall } from '../components/MysticBall';
 import styles from './page.module.scss';
+import React from 'react';
+import Image from 'next/image';
+import { GITHUB_REPO_URL } from '@/constants/common';
 
 export default function Home() {
   return (
     <div className={styles.root}>
       <MysticBall />
+      <a href={GITHUB_REPO_URL} target="_blank" rel="noopener noreferrer">
+        <Image
+          className={styles.github}
+          src="github-mark-white.svg"
+          alt="github"
+          width="42"
+          height="42"
+        />
+      </a>
     </div>
   );
 }
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,2 +1,4 @@
 export const TELEGRAM_WEP_APP_SCRIPT_URL =
   'https://telegram.org/js/telegram-web-app.js';
+
+export const GITHUB_REPO_URL = 'https://github.com/iypavlov/tma-mystic-ball';
